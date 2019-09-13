4a928849496b400bbeacab66094a0e90cb2ac1cc
RednaU <uandermatheus2015@gmail.com>
2019-09-12 21:58:53 -0300

    Criar aluno

---
+++ Escola/src/Escola/CriarAluno.java
@@ -0,0 +1,15 @@
+package Escola;
+
+public class CriarAluno{
+
+	public static void main(String[] args) {
+      
+		Aluno aluno1 = new Aluno();
+		aluno1.nome = "Juliana";
+		aluno1.idade = 20;
+		aluno1.nota1 = 9;
+		aluno1.nota2 = 10;
+
+	}
+
+}
